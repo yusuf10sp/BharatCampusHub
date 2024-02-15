d8b214647a78ab4a3b3dc641f818232a91d087e6
yusuf10sp <md.yusuf786dns@gmail.com>
2024-02-15 10:29:39 +0530

    done

--- form handler.php
+++ form handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message']
 
-$email_from = 'yusuf@yourwebsite.com'
+$email_from = 'https://yusuf10sp.github.io/BharatCampusHub/'
 
 $email_subject = 'New Form Submission';
 
